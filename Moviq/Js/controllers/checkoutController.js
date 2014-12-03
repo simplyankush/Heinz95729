7fb42114276d7a4a685dc36929e19ad6e89a2474
mcleinman <mcleinma@andrew.cmu.edu>
2014-12-03 17:30:58 -0500

    Improved UI, removed alerts

--- Moviq/Js/controllers/checkoutController.js
+++ Moviq/Js/controllers/checkoutController.js
@@ -5,23 +5,34 @@ define('controllers/checkoutController', {
     "use strict";
 
     
-routes.get('/#/checkout', function (context) {
-    viewEngine.setView({
-  template: 't-checkout',
-  data: {
-      testcart: [
-             { title: 'Sample Book', detailsLink: 'http://www.google.com', price: '4.99' },
-             { title: 'Sample Book: The Sequel', detailsLink: 'http://www.yahoo.com', price: '5.99' },
-             { title: 'Sample Book: The Exciting End of the Trilogy', detailsLink: 'http://www.bing.com', price: '6.99' },
-             
-      ],
-
-      totalItem: '3',
-      totalPrice: '17.97'
-  }
-   });
-});
+        routes.get('/#/checkout', function (context) {
+            viewEngine.setView({
+                template: 't-checkout',
+                data: {
+                    testcart: [
+                            { thumbnailLink: '/images/books/beforeIGo.jpg', title: 'Sample Book', detailsLink: 'http://www.google.com', price: '4.99' },
+                            { thumbnailLink: '/images/books/beforeIGo.jpg', title: 'Sample Book: The Sequel', detailsLink: 'http://www.yahoo.com', price: '5.99' },
+                            { thumbnailLink: '/images/books/beforeIGo.jpg', title: 'Sample Book: The Exciting End of the Trilogy', detailsLink: 'http://www.bing.com', price: '6.99' }
+                    ],
+                    total: ko.computed(function(theCart){
+                        var total = 0;
+                        for (var item in theCart) {  // not hitting this loop
+                                total += item.price;
+                                alert(item.price);
+                        };
+                        //ko.utils.arrayForEach(theCart, function (item) {
+                        //    var value = parseFloat(item.price());
+                        //    if (!isNaN(value)) {
+                        //        total += value;
+                        //    }
+                        //});
+                        return total;
+                    }, this.testcart),
+                    totalItem: 100
+                 }
+           });
+        });
 
     
-}
+    }
 });
