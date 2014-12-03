b0fbfc74514bc6283a04b10e27eda4ce26ca1ed8
mcleinman <mcleinma@andrew.cmu.edu>
2014-12-02 22:40:27 -0500

    Nearly done with checkout page
    Total function in checkoutController,.js not working.  Need to add unit
    tests.

--- Moviq/Js/controllers/checkoutController.js
+++ Moviq/Js/controllers/checkoutController.js
@@ -7,15 +7,29 @@ define('controllers/checkoutController', {
     
 routes.get('/#/checkout', function (context) {
     viewEngine.setView({
-  template: 't-checkout',
-  data: {
-      testcart: [
-             { title: 'Sample Book', detailsLink: 'http://www.google.com', price: '4.99' },
-             { title: 'Sample Book: The Sequel', detailsLink: 'http://www.yahoo.com', price: '5.99' },
-             { title: 'Sample Book: The Exciting End of the Trilogy', detailsLink: 'http://www.bing.com', price: '6.99' }
-      ]
-  }
-   });
+        template: 't-checkout',
+        data: {
+            testcart: [
+                 { thumbnailLink: '/images/books/beforeIGo.jpg', title: 'Sample Book', detailsLink: 'http://www.google.com', price: '4.99' },
+                 { thumbnailLink: '/images/books/beforeIGo.jpg', title: 'Sample Book: The Sequel', detailsLink: 'http://www.yahoo.com', price: '5.99' },
+                 { thumbnailLink: '/images/books/beforeIGo.jpg', title: 'Sample Book: The Exciting End of the Trilogy', detailsLink: 'http://www.bing.com', price: '6.99' }
+            ],
+            total: ko.computed(function(theCart){
+                var total = 0;
+                for (var item in theCart) {  // not hitting this loop
+                    total += item.price;
+                    alert(item.price);
+                };
+                //ko.utils.arrayForEach(theCart, function (item) {
+                //    var value = parseFloat(item.price());
+                //    if (!isNaN(value)) {
+                //        total += value;
+                //    }
+                //});
+                return total;
+            }, this.testcart)
+        }
+    });
 });
 
     
