af9e8c5314823f2535359f21d30aa6cbdf66073a
Ankush Grover <simplyankush@gmail.com>
2014-12-03 16:09:06 -0500

    Backend Code to add product to cart

--- Moviq/Js/controllers/cartController.js
+++ Moviq/Js/controllers/cartController.js
@@ -1,1 +1,11 @@
-﻿
+﻿define('controllers/cartController', { init: function (routes, viewEngine, Products, Product, Cart) {
+    "use strict";
+
+    // GET /books/search/?q=searchterm
+    // search for a book or books
+    routes.get(/^\/#\/cart\/add\/?/i, function (context) { 
+        $.ajax({
+            url: '/api/cart/add/' + context.params.q,
+            method: 'GET'
+        }).done(function (data) {  
+    
