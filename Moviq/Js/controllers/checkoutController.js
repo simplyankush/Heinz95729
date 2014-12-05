236f595bcab93e01601a340d6cfdae6ccd84428f
mcleinman <mcleinma@andrew.cmu.edu>
2014-12-05 14:31:43 -0500

    Total and # objects works again!

--- Moviq/Js/controllers/checkoutController.js
+++ Moviq/Js/controllers/checkoutController.js
@@ -10,10 +10,13 @@ define('controllers/checkoutController', {
                 url: '/api/cart/full',
                 method: 'GET'
             }).done(function (data) {
-                var books = new Books (JSON.parse(data));
+                var books = new Books(JSON.parse(data));
+
                 //alert(String(books));
                 var dataModel = function (data) {
+                 
                     var self = {};
+
 
                     //self.testcart = [    // later changes to the live data
                     //                   { thumbnailLink: '/images/books/beforeIGo.jpg', title: 'Sample Book', detailsLink: 'http://www.google.com', price: 4.99 },
@@ -25,17 +28,19 @@ define('controllers/checkoutController', {
 
                     self.totalPrice = ko.computed(function () {
                         var total = 0, i = 0, current;
-            
-                        for (i; i<self.testcart.length; i++) {
-                            current = self.testcart[i];
-                            total += current.price;
+
+                        
+
+                        for (i; i < self.testcart().length; i++) {
+                            current = self.testcart()[i];
+                            total += current.price();
                         };
            
                         return total;
                     });
 
                     self.totalItem = ko.computed(function () {
-                        return self.testcart.length;
+                        return self.testcart().length;
                     });
 
                     return self;
